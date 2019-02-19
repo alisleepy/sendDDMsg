ba00d93347b24ce02fe2bdd866a62456644e2045
wangkaikai <wangkk@sunits.com>
2019-02-19 15:59:33 +0800

    初始化提交

---
+++ config.php
@@ -0,0 +1,6 @@
+<?php
+define('DIR_ROOT', dirname(__FILE__).'/');
+define("OAPI_HOST", "https://oapi.dingtalk.com");
+//Suite
+define("APP_KEY", "ding4nzeqgzire3xczee");
+define("APP_SECRET", "R4nuPMEBTMKdrlHrELM16Zh-oYM8s_REiOu6lXX5D7L1JsUN2iXWMgVZkenWz-PL");
